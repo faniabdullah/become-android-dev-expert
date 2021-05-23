bbb0aae9232ea3ba50b51e7f5617cd34c1007bd4
faniabdullah <faniabdullah27@gmail.com>
2021-05-23 23:12:16 +0700

    encript data

--- core/src/main/java/com/bangkit/faniabdullah_made/core/di/DatabaseModule.kt
+++ core/src/main/java/com/bangkit/faniabdullah_made/core/di/DatabaseModule.kt
@@ -9,6 +9,8 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
 import javax.inject.Singleton
 
 
@@ -17,10 +19,16 @@ import javax.inject.Singleton
 class DatabaseModule {
     @Singleton
     @Provides
-    fun provideDatabase(@ApplicationContext context: Context): MovieDatabase = Room.databaseBuilder(
-        context,
-        MovieDatabase::class.java, "Movie.db"
-    ).fallbackToDestructiveMigration().build()
+    fun provideDatabase(@ApplicationContext context: Context): MovieDatabase {
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("faniabdullah".toCharArray())
+        val factory = SupportFactory(passphrase)
+        return Room.databaseBuilder(
+            context,
+            MovieDatabase::class.java, "Movie.db"
+        ).fallbackToDestructiveMigration()
+            .openHelperFactory(factory)
+            .build()
+    }
 
     @Provides
     fun provideMovieDao(database: MovieDatabase): MovieDao = database.movieDao()
